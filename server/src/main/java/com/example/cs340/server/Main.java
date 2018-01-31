026796b98274f23e1ba44866b6764c7a2c511a48
morme2 <matt.orme2010@gmail.com>
2018-01-31 16:42:11 -0700

    testing my upload

--- server/src/main/java/com/example/cs340/server/Main.java
+++ server/src/main/java/com/example/cs340/server/Main.java
@@ -3,5 +3,6 @@ package com.example.cs340.server;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello server");
+        System.out.println("and Hello World");
     }
 }

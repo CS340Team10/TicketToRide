1a5eb458ea5e7c44f3303096194fb4385e121ec8
Brian Millerberg <bmillerberg@byu.net>
2018-02-05 16:51:17 -0700

    Included static Endpoint variables

---
+++ common/src/main/java/common/Endpoints.java
@@ -0,0 +1,13 @@
+package common;
+
+/**
+ * Created by Brian on 2/5/18.
+ */
+
+public class Endpoints {
+
+    public static final String DEFAULT_ENDPOINT          = "/";
+    public static final String GAME_LIST_ENDPOINT        = "/gameList/";
+    public static final String POLL_ENDPOINT             = "/poll/";
+    public static final String EXEC_COMMAND_ENDPOINT     = "/execCommand/";
+}

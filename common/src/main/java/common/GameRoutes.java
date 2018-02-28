1f5110c21fc70b9447a2cb05a8067d612de4b5dc
Ephraim Kunz <ephraimkunz@me.com>
2018-02-28 15:55:55 -0700

    Create static GameRoutes in common

---
+++ common/src/main/java/common/GameRoutes.java
@@ -0,0 +1,14 @@
+package common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ephraimkunz on 2/28/18.
+ */
+
+public class GameRoutes {
+    public static List<Route> getAllRoutes() {
+        return new ArrayList<>();
+    }
+}

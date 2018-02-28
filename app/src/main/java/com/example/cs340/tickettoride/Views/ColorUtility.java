07e98b88c421d692b48481a7e7c8b907326707bb
Joseph <jmiera@byu.edu>
2018-02-28 16:28:48 -0700

    Implemented ColorUtility

---
+++ app/src/main/java/com/example/cs340/tickettoride/Views/ColorUtility.java
@@ -0,0 +1,79 @@
+package com.example.cs340.tickettoride.Views;
+
+import com.example.cs340.tickettoride.R;
+
+import common.ICard;
+import common.PlayerAttributes;
+import common.TrainCard;
+
+/**
+ * Created by Joseph on 2/28/2018.
+ */
+
+public class ColorUtility
+{
+    public class CardColors
+    {
+        CardColors(int backColor, int textColor)
+        {
+            this.backColor = backColor;
+            this.textColor = textColor;
+        }
+        private int backColor = R.color.colorGrey;
+        private int textColor = R.color.colorBlack;
+    }
+
+    public CardColors getColorsFromCard(ICard card)
+    {
+        if (card.getClass() == TrainCard.class)
+        {
+            TrainCard tCard = (TrainCard) card;
+            TrainCard.Colors tColor = tCard.getColor();
+            switch (tColor)
+            {
+                case red:
+                    return new CardColors(R.color.colorRed, R.color.colorWhite);
+                case green:
+                    return new CardColors(R.color.colorGreen, R.color.colorBlack);
+                case blue:
+                    return new CardColors(R.color.colorBlue, R.color.colorWhite);
+                case white:
+                    return new CardColors(R.color.colorWhite, R.color.colorBlack);
+                case black:
+                    return new CardColors(R.color.colorBlack, R.color.colorWhite);
+                case purple:
+                    return new CardColors(R.color.colorPurple, R.color.colorWhite);
+                case orange:
+                    return new CardColors(R.color.colorOrange, R.color.colorBlack);
+                case yellow:
+                    return new CardColors(R.color.colorYellow, R.color.colorBlack);
+                case wildcard:
+                    return new CardColors(R.color.colorPink, R.color.colorBlack);
+                case none:
+                    return new CardColors(R.color.colorGrey, R.color.colorBlack);
+                default:
+                    return new CardColors(R.color.colorGrey, R.color.colorBlack);
+            }
+        }
+        return  new CardColors(R.color.colorGrey, R.color.colorBlack);
+    }
+
+    private int getColorFromPlayer(PlayerAttributes.Color color)
+    {
+        switch (color)
+        {
+            case red:
+                return R.color.colorRed;
+            case green:
+                return R.color.colorGreen;
+            case blue:
+                return R.color.colorBlue;
+            case black:
+                return R.color.colorBlack;
+            case yellow:
+                return R.color.colorYellow;
+            default:
+                return R.color.colorGrey;
+        }
+    }
+}

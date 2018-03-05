ad3dc12cfebead0bd015bd46b72dad0614e429fc
Joseph <jmiera@byu.edu>
2018-03-04 22:43:55 -0700

    Fixed error in ColorUtility where it was returning resource IDs instead of color values. Fixed HandView so that it works. Added HandPresenter and IHandPresenter. Modified Manifest for testing.

---
+++ app/src/main/java/Presenters/HandPresenter.java
@@ -0,0 +1,55 @@
+package Presenters;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.example.cs340.tickettoride.Views.IHandView;
+
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+import ClientModel.ClientModel;
+import common.Deck;
+import common.DestCard;
+import common.TrainCard;
+
+/**
+ * Created by Joseph on 3/2/2018.
+ */
+
+public class HandPresenter implements IHandPresenter, Observer
+{
+    private final String TAG = "HandPresenter";
+    private IHandView handView;
+
+    public HandPresenter(IHandView handView)
+    {
+        this.handView = handView;
+        ClientModel.getInstance().addObserver(this);
+    }
+
+    @Override
+    public void onShowHand()
+    {
+        //Do nothing for now
+    }
+
+    @Override
+    public void update(Observable o, Object arg)
+    {
+        if (handView != null) {
+            ClientModel model = (ClientModel) o;
+            Deck destHand = model.getUser().getDestCards();
+            List<DestCard> dCards = (List<DestCard>) destHand.toList(DestCard.class);
+            handView.updateDestHand(dCards);
+            Deck trainHand = model.getUser().getTrainCards();
+            List<TrainCard> tCards = (List<TrainCard>) trainHand.toList(TrainCard.class);
+            handView.updateTrainHand(tCards);
+        }
+        else
+        {
+            Log.e(TAG, "ERROR: cannot update! The HandPresenter has a NULL HandView");
+        }
+    }
+}

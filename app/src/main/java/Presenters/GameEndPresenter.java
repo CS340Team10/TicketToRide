234e7728b811548d84286031d6e2e4e029aac62d
Joseph <jmiera@byu.edu>
2018-03-23 16:49:12 -0600

    Modified presenter to replace the player ID's with usernames after getting them, that way the view can display usernames instead.

--- app/src/main/java/Presenters/GameEndPresenter.java
+++ app/src/main/java/Presenters/GameEndPresenter.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.example.cs340.tickettoride.Views.IGameEndView;
 
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 import ClientModel.ClientModel;
+import ClientModel.Player;
+import common.PlayerPointSummary;
 
 /**
  * Created by Joseph on 3/20/2018.
@@ -32,6 +35,15 @@ public class GameEndPresenter implements IGameEndPresenter, Observer
     @Override
     public void update(Observable o, Object arg)
     {
-        gameEndView.loadPointSummary(ClientModel.getInstance().getPointSummaries());
+        List<PlayerPointSummary> pointSummaries = ClientModel.getInstance().getPointSummaries();
+        for (PlayerPointSummary pps : pointSummaries)
+        {
+            Player p = ClientModel.getInstance().getPlayerByID(pps.getPlayerId());
+            if (p != null)
+            {
+                pps.setPlayerId(p.getUsername());
+            }
+        }
+        gameEndView.loadPointSummary(pointSummaries);
     }
 }

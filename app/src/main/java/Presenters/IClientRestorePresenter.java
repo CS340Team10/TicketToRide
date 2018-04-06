d8fefe082819e32c3cc788cfa06d0037f909a245
morme2 <matt.orme2010@gmail.com>
2018-04-06 16:03:47 -0600

    Client Restore Classes

---
+++ app/src/main/java/Presenters/IClientRestorePresenter.java
@@ -0,0 +1,10 @@
+package Presenters;
+
+/**
+ * Created by matto on 4/6/2018.
+ */
+
+public interface IClientRestorePresenter {
+    public void beginRestore();
+    public void onPostRestore();
+}
---
+++ app/src/main/java/com/example/cs340/tickettoride/Views/IClientRestoreView.java
@@ -0,0 +1,9 @@
+package com.example.cs340.tickettoride.Views;
+
+/**
+ * Created by matto on 4/6/2018.
+ */
+
+public interface IClientRestoreView {
+    public void displayMessage(String message);
+}
